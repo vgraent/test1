63a189a7ca98181062a87823a1e2423832448b57
raent25 <raent25@gmail.com>
2019-07-30 16:08:20 +0300

    Update index.php
    удалил старый не нужный код

--- index.php
+++ index.php
@@ -3,32 +3,7 @@ $author = "Scott Adams";
 
  echo ' <h1> TEST</h1>';
  echo "$author";
- for ($count= 1; $count <10; $count++)
- {
-       if ($count == 5)        continue;
-     echo "$count";
-    
- }
- ///
-function tet ($a)
- {
-    $b =$a +10;
-    return $b;
- }
- $c = tet(15);
- echo "<br>$c";
- ///
- $name = name("Well", "will", "WALL");
- echo $name[0]. " ".$name[1]." ".$name[2];
- function name($n1,$n2,$n3)
- {
-     $n1 = ucfirst(strtolower($n1));
-     $n2 = ucfirst(strtolower($n2));
-     $n3 = ucfirst(strtolower($n3));
-     return array($n1, $n2, $n3);
-     
- }
- echo "<br>";
+ 
  ///
   
    require_once 'test.php';
